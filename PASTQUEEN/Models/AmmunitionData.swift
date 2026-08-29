9688922072002e0cee1fc5dd93a3085236f31455
Momo L'As <momolas@users.noreply.github.com>
2026-08-29 14:45:23 +0200

    Feat: add Savage Axis and Axis Varmint models to embedded weapon catalog

--- PASTQUEEN/Models/AmmunitionData.swift
+++ PASTQUEEN/Models/AmmunitionData.swift
@@ -201,6 +201,13 @@ struct AmmunitionData {
 
         // Savage
         CatalogWeapon(manufacturer: "Savage", name: "B22 Precision", caliber: ".22 LR", twistRateInches: 16.0, twistDirection: .right, sightHeightCM: 4.0, defaultScopeUnit: .moa18, category: "Châssis MDT Rimfire"),
+        CatalogWeapon(manufacturer: "Savage", name: "B22 F / FV-SR", caliber: ".22 LR", twistRateInches: 16.0, twistDirection: .right, sightHeightCM: 3.8, defaultScopeUnit: .moa18, category: "Verrou Rimfire"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis Varmint", caliber: ".308 Win", twistRateInches: 10.0, twistDirection: .right, sightHeightCM: 4.5, defaultScopeUnit: .moa18, category: "Canon Lourd Varmint"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis Varmint", caliber: ".223 Rem", twistRateInches: 9.0, twistDirection: .right, sightHeightCM: 4.5, defaultScopeUnit: .moa18, category: "Canon Lourd Varmint"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis / Axis II", caliber: ".308 Win", twistRateInches: 10.0, twistDirection: .right, sightHeightCM: 3.8, defaultScopeUnit: .moa14, category: "Verrou Standard"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis / Axis II", caliber: ".223 Rem", twistRateInches: 9.0, twistDirection: .right, sightHeightCM: 3.8, defaultScopeUnit: .moa14, category: "Verrou Standard"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis / Axis II", caliber: "6.5 Creedmoor", twistRateInches: 8.0, twistDirection: .right, sightHeightCM: 3.8, defaultScopeUnit: .moa14, category: "Verrou Standard"),
+        CatalogWeapon(manufacturer: "Savage", name: "Axis / Axis II", caliber: ".30-06", twistRateInches: 10.0, twistDirection: .right, sightHeightCM: 3.8, defaultScopeUnit: .moa14, category: "Verrou Standard"),
         CatalogWeapon(manufacturer: "Savage", name: "Axis II Precision", caliber: ".308 Win", twistRateInches: 10.0, twistDirection: .right, sightHeightCM: 4.0, defaultScopeUnit: .moa18, category: "Châssis MDT TLD"),
         CatalogWeapon(manufacturer: "Savage", name: "Axis II Precision", caliber: "6.5 Creedmoor", twistRateInches: 8.0, twistDirection: .right, sightHeightCM: 4.0, defaultScopeUnit: .moa18, category: "Châssis MDT TLD"),
         CatalogWeapon(manufacturer: "Savage", name: "110 Elite Precision", caliber: ".308 Win", twistRateInches: 10.0, twistDirection: .right, sightHeightCM: 4.5, defaultScopeUnit: .mrad10, category: "Compétition PRS / ELR"),
